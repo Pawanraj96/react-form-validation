a2bcac4ec16cc27c552443c299a907666518d1ff
Pawanraj96 <rajpawan1106@gmail.com>
2022-01-22 10:53:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './components/Register';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Register/>
+    </div>
+  );
+}
+
+export default App;
